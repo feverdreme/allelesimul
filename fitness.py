7dde2736380f7b23777a1db9865a1b85c5a5d806
saute-fest <56605759+saute-fest@users.noreply.github.com>
2022-05-13 12:52:16 -0400

    Update fitness.py

--- fitness.py
+++ fitness.py
@@ -6,14 +6,14 @@ def fitness(genome):
         fitness = 0
 
         #get each allele from genome
-        genes = list(genome[i])
+        allele = list(genome[i])
 
         #loop through allele
-        for g in range(len(genes)):
+        for g in range(len(allele)):
 
             #if presence of positive fitness gene is detected, add one to fittnes. 
             #+= wouldnt work because AA would return a fitness of 2 instead of 1. Having two copies of positive fitness train does not actually increase the overall fitness
-            if genes[g] == 'A':
+            if allele[g] == 'A':
                 fitness = 1
         #append result of fitness into array
         fitarray.append(fitness)
