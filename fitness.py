e59004df66bdbc0b21465fdcdbcce536f4595022
saute-fest <56605759+saute-fest@users.noreply.github.com>
2022-05-13 22:05:01 -0400

    added phenotypes
    Added phenotypes that can be randomized for fitness and dominance. fitness function modified accordingly.

--- fitness.py
+++ fitness.py
@@ -1,26 +1,29 @@
+import phenotype
+
+phenotype.setFitness()
+phenotype.setDominance()
+
 def fitness(genome):
+    
     fitarray = []
 
     #loop through genome
     for i in range(len(genome)):
+        
         fitness = 0
 
-        #get each allele from genome
-        allele = list(genome[i])
+        #searches through phenotype dictionary to find fitness score
+        fitness = phenotype.findFitness(genome[i])
 
-        #loop through allele
-        for g in range(len(allele)):
-
-            #if presence of positive fitness gene is detected, add one to fittnes. 
-            #+= wouldnt work because AA would return a fitness of 2 instead of 1. Having two copies of positive fitness train does not actually increase the overall fitness
-            if allele[g] == 'A':
-                fitness = 1
         #append result of fitness into array
         fitarray.append(fitness)
 
     #add together the fitnesses of every allele to get the organisms total fitness
+    
     fitscore = sum(fitarray)
+
     return fitscore
+
 
 
 # theoretically the final solution would be a list generated before the simulation where fitness of each phenotype is decided randomly. This function would then just check the phenotype agains the list and retrive its fitness score
---
+++ phenotype.py
@@ -0,0 +1,71 @@
+import random
+
+#dictionaries
+red = {
+    "trait": "Red Color",
+    "allele": [],
+    "dominance": True,
+    "fitness": 0
+}
+
+
+yellow = {
+    "trait": "Yellow Color",
+    "allele": [],
+    'dominance': True,
+    "fitness": 0
+}
+
+
+#applies a random number 0 to 99 to each phenotype's fitness
+def setFitness():
+
+    yellow["fitness"] = random.randint(0,99)
+
+    red["fitness"] = random.randint(0,99)
+
+
+#randomly chooses either 0 or 1, if one is selected, yellow is set to dominant trait, if 0 is selected red is set to dominant
+def setDominance():
+
+    num = random.randint(0,1)
+    
+    if num == 1:
+        
+        yellow["allele"] = ["AA", "Aa", "aA"]
+        
+        red["allele"] = ["aa"]
+    
+    else:
+        
+        red["allele"] = ["AA", "Aa", "aA"]
+        
+        yellow["allele"] = ["aa"]   
+    
+    print(yellow["allele"], red["allele"])
+
+
+#setFitness and setDominance should be run at the begining of every simulation and run only once
+
+
+#can only currently take in args of AA, Aa, or aA (im not sure if the proccess you are doing would generate a ration of 1 AA : 2 Aa : 1 aa or 1 AA : 1 Aa : 1 aA : 1 aa)
+def findFitness(input):
+
+    #loops through the range of yellow's possible alleles and checks if the input allele defines the phenotype
+        for i in range(len(yellow["allele"])):
+
+            #if input matches a definition of the phenotype's allele(s), return the fitness score
+            if input == yellow["allele"][i]:
+                
+                return yellow['fitness']
+        
+        for g in range(len(red["allele"])):
+            
+            if input == red["allele"][g]:
+                
+                return red['fitness']
+
+
+
+
+
